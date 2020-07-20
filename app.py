9ed756c5261e9d11c438f823fac49a6d22c81dcf
Beau Batchelor <beauwbatchelor@gmail.com>
2020-07-19 21:55:21 -0700

    adding drag and drop functionality to values page

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template
 from flask_pymongo import PyMongo
 import datetime as dt
 from config import user, psw
-# from tables import Customer_site_dump, db
 
-app.config["MONGO_URI"] = f"mongodb+srv://{user}:{psw}@valutask.wqycs.azure.mongodb.net/Site"
-mongo = PyMongo(app)
+# app.config["MONGO_URI"] = f"mongodb+srv://{user}:{psw}@valutask.wqycs.azure.mongodb.net/Site" ## production connection
+# mongo = PyMongo(app)
 
+test_data = {"_id":{"$oid":"5f0a6b880c689113c019b670"},"user":"test_user","values_data":[{"catagory":"Maslow","values":["Physiological Needs","Safety","Belongingness and Love","Esteem","Self Actualization"]},{"catagory":"Urgency","values":["Urgent","Neutral","Not Urgent"]},{"catagory":"Interest","values":["Technology","Wealth","Friendship","Health"]}],"org_data":["Life","Work","Project"],"task_data":{"columns":["Maslow","Urgency","Interest","Due Date","Task","Status (Optional)","Assigned To (Optional)"],"values":[["Esteem","Netral","Health","2099-7-8","Creat a workout schedule","not started","Beau"],["Physiological Needs","Urgent","Health","2020-8-1","Groceries","not started","Beau"],["Belongingness and Love","Neutral","Friendship","2020-12-4","420Bot","not started","Beau"],["Saftey","Neutral","Health","2020-8-1","Pay Rent","not started","Beau"]]}}
 
 @app.route('/')
 def index():
@@ -16,21 +16,25 @@ def index():
 
 @app.route('/organizations', methods=('POST', 'GET'))
 def organizations():
-    user_data = mongo.db.User.find_one({'user': 'test_user'})
+    # user_data = mongo.db.User.find_one({'user': 'test_user'}) ##production data
+    user_data = test_data ##for test data
     org_data = user_data['org_data']
+    
 
     return render_template('organization.html', data=org_data)
 
 @app.route('/values', methods=('POST', 'GET'))
 def values():
-    user_data = mongo.db.User.find_one({'user': 'test_user'})
+    # user_data = mongo.db.User.find_one({'user': 'test_user'}) ##production data
+    user_data = test_data ##for test data  
     values_data = user_data['values_data']
 
     return render_template('values.html', data=values_data)
 
 @app.route('/tasks', methods=('POST', 'GET'))
 def tasks():
-    user_data = mongo.db.User.find_one({'user': 'test_user'})
+    # user_data = mongo.db.User.find_one({'user': 'test_user'}) ##production data
+    user_data = test_data ##for test data
     task_data = user_data['task_data']
 
     return render_template('tasks.html', data=task_data)
---
+++ static/script.js
@@ -0,0 +1,41 @@
+const valuesDrag = document.querySelectorAll('.value-draggable')
+const tables = document.querySelectorAll('tbody')
+
+valuesDrag.forEach(valuesDrag => {
+    valuesDrag.addEventListener('dragstart', () => {
+        valuesDrag.classList.add('dragging')
+        console.log(valuesDrag.outerHTML)
+    })
+
+    valuesDrag.addEventListener('dragend', () => {
+        valuesDrag.classList.remove('dragging')     
+        console.log(valuesDrag.outerHTML)
+    })
+})
+
+tables.forEach(table => {
+    table.addEventListener('dragover', e => {
+        e.preventDefault()
+        const afterElement = moveValue(table, e.clientY)
+        const draggable = document.querySelector('.dragging')
+        if (afterElement == null) {
+            table.appendChild(draggable)
+          } else {
+            table.insertBefore(draggable, afterElement)
+          }
+    })
+})
+
+function moveValue(table, y) {
+    const draggableValues = [...table.querySelectorAll('.value-draggable:not(.dragging)')]
+    
+    return draggableValues.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child }
+        } else {
+          return closest
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+}
