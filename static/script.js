cc6ce3a49351ad419270ab48b5becd6cb28b3fbf
Beau Batchelor <beauwbatchelor@gmail.com>
2020-07-26 22:09:32 -0700

    adding attempt at edit and save button in js

--- static/script.js
+++ static/script.js
@@ -4,14 +4,29 @@ const valueRows = document.querySelectorAll('div#value-row')
 
 
 valueRows.forEach(valueRow => {
-  let valueTextEle = valueRow.querySelector('div#value-changeable')
-  let valueButtonEle = valueRow.querySelector('button#value-edit-button')
-  let valueText = valueTextEle.innerHTML
-  
-  valueButtonEle.addEventListener('click', () => {
-    valueTextEle.outerHTML = '<div class="col-9" id="value-changeable"><input type="text" placeholder="' + valueText + '"></div>'
-    valueButtonEle.outerHTML = '<button id="value-save-button" type="button">Save</button>'
-  })
+  let counter = 1
+  if (counter % 2) {
+    let valueTextEle = valueRow.querySelector('div#value-changeable')
+    let valueButtonEditEle = valueRow.querySelector('button#value-edit-button')
+    let valueText = valueTextEle.innerHTML
+    
+    valueButtonEditEle.addEventListener('click', () => {
+      valueTextEle.outerHTML = '<div class="col-9" id="value-changeable"><input type="text" placeholder="' + valueText + '"></div>'
+      valueButtonEditEle.outerHTML = '<button id="value-save-button" type="button">Save</button>'
+    })
+
+    counter = ++counter
+  } else {
+    let valueButtonSaveEle = valueRow.querySelector('button#value-save-button')
+    let valueChangeTextEle = valueRow.querySelector('div#value-changeable')
+    let valueChangeTex = valueChangeTextEle.innerHTML
+
+    valueButtonSaveEle.addEventListener('click', () => {
+      valueChangeTextEle.outerHTML = '<div class="col-9" id="value-changeable"><h5>"' + valueChangeTex + '"</h5></div>'
+      valueButtonSaveEle.outerHTML = '<button id="value-edit-button" type="button">Edit</button>'
+    })
+    counter = ++counter
+  }
 })
 
 
