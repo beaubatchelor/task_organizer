79846f6713d55f96f04bd139074605ed02403095
Beau Batchelor <beauwbatchelor@gmail.com>
2020-07-27 21:22:27 -0700

    adding completed edit button

--- static/script.js
+++ static/script.js
@@ -2,32 +2,38 @@ const valuesDrag = document.querySelectorAll('.value-draggable')
 const tables = document.querySelectorAll('#value-body')
 const valueRows = document.querySelectorAll('div#value-row')
 
+function editClick(valueRow) {
+  let valueTextEleEdit = valueRow.querySelector('div#value-changeable')
+  let valueButtonEleEdit = valueRow.querySelector('button#value-edit-button')
+  let valueTextEdit = valueTextEleEdit.innerHTML
+
+  valueTextEleEdit.outerHTML = '<div class="col-9" id="value-changeable"><input id="value-changeable-input" type="text" placeholder="' + valueTextEdit + '"></div>'
+  valueButtonEleEdit.outerHTML = '<button id="value-save-button" type="button">Save</button>'
+
+  let newButtonEle = valueRow.querySelector('button#value-save-button')
+  newButtonEle.addEventListener('click', () => {saveClick(valueRow)});
+};
+
+function saveClick(valueRow) {
+  let valueTextEleSave = valueRow.querySelector('div#value-changeable')
+  let valueButtonEleSave = valueRow.querySelector('button#value-save-button')
+  let valueTextSave = valueRow.querySelector('input#value-changeable-input').value
+
+  valueTextEleSave.outerHTML = '<div class="col-9" id="value-changeable">' + valueTextSave + '</div>'
+  valueButtonEleSave.outerHTML = '<button id="value-edit-button" type="button">Edit</button>'
+
+  let newButtonEle = valueRow.querySelector('button#value-edit-button')
+  newButtonEle.addEventListener('click', () => {editClick(valueRow)});
+};
+
+
 
 valueRows.forEach(valueRow => {
-  let counter = 1
-  if (counter % 2) {
-    let valueTextEle = valueRow.querySelector('div#value-changeable')
-    let valueButtonEditEle = valueRow.querySelector('button#value-edit-button')
-    let valueText = valueTextEle.innerHTML
-    
-    valueButtonEditEle.addEventListener('click', () => {
-      valueTextEle.outerHTML = '<div class="col-9" id="value-changeable"><input type="text" placeholder="' + valueText + '"></div>'
-      valueButtonEditEle.outerHTML = '<button id="value-save-button" type="button">Save</button>'
-    })
-
-    counter = ++counter
-  } else {
-    let valueButtonSaveEle = valueRow.querySelector('button#value-save-button')
-    let valueChangeTextEle = valueRow.querySelector('div#value-changeable')
-    let valueChangeTex = valueChangeTextEle.innerHTML
-
-    valueButtonSaveEle.addEventListener('click', () => {
-      valueChangeTextEle.outerHTML = '<div class="col-9" id="value-changeable"><h5>"' + valueChangeTex + '"</h5></div>'
-      valueButtonSaveEle.outerHTML = '<button id="value-edit-button" type="button">Edit</button>'
-    })
-    counter = ++counter
-  }
+  const valueButtonEle = valueRow.querySelector('button#value-edit-button')
+  valueButtonEle.addEventListener('click', () => {editClick(valueRow)})
 })
+
+
 
 
 valuesDrag.forEach(valuesDrag => {
